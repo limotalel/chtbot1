88ab5c3887ba3d8bd93c3051102fc8766bef4477
limotalel <107612749+limotalel@users.noreply.github.com>
2023-12-16 13:14:42 +0300

    utils.py

--- ChartBot By Twilio for Whatsapp/utils.py
+++ ChartBot By Twilio for Whatsapp/utils.py
@@ -6,10 +6,10 @@ from twilio.rest import Client
 import decouple
 
 # Find your Account SID and Auth Token at twilio.com
-account_sid = "AC0889071abbe467be25e3005281b7a521"
-auth_token = "9ca07ec985199fa4b217ef8cb3065699"
+account_sid = ""
+auth_token = ""
 client = Client(account_sid, auth_token)
-twilio_number = "+14155238886"
+twilio_number = ""
 
 # Set up logging
 logging.basicConfig(level=logging.INFO)
